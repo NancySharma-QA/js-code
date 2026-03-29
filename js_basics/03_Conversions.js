d448e56540eacfc37f53e22971f795cf3946f208
NancySharma-QA <sharmanancy1208@gmail.com>
2026-03-29 08:55:49 +0000

    container config done

--- js_basics/03_Conversions.js
+++ js_basics/03_Conversions.js
@@ -32,7 +32,7 @@ let stringNumber = String(someNumber)
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+// console.log(negValue); //output = -3
 
 // console.log(2+2);
 // console.log(2-2);
@@ -47,22 +47,22 @@ let str2 = " hitesh"
 let str3 = str1 + str2
 // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2); output = 12
+// console.log(1 + "2"); output = 12
+// console.log("1" + 2 + 2); output = 122
+// console.log(1 + 2 + "2"); output = 32
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+// console.log(+true); output = 1, if print only true then o/p will be true
+// console.log(+""); output = 0
 
 let num1, num2, num3
 
 num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
-++gameCounter;
+++gameCounter; //output = 101, game counter ++ = 101
 console.log(gameCounter);
 
 // link to study
