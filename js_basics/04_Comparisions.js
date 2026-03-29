12bbf50757d97c74d67e99a9f41f609880260146
NancySharma-QA <sharmanancy1208@gmail.com>
2026-03-29 12:18:17 +0000

    container config done

--- js_basics/04_Comparisions.js
+++ js_basics/04_Comparisions.js
@@ -5,17 +5,17 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("2" > 1); Output = true, automatically it converts into number
+// console.log("02" > 1); Output = true, automatically it converts into number
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0); //Output = false
+console.log(null == 0); //Output = false
+console.log(null >= 0); //Output = true
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined == 0); //Output = false
+console.log(undefined > 0); //Output = false
+console.log(undefined < 0); //Output = false Avoid these above null undefined type conversions
 
-// === 
+// === it checks value strictly i.e it check values as well as data types too
 
-console.log("2" === 2);
+console.log("2" === "2"); //Output = false
